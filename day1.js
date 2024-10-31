58097882e420068d58a3656192e5e5a6d8780ac4
musharraf504 <124203520+musharraf504@users.noreply.github.com>
2024-11-01 00:51:36 +0530

    day2 update

---
+++ day1.js
@@ -0,0 +1,29 @@
+// Iterating using forEach and for loop
+
+// let a_dumb_array = ["hsdbvjhsf","hjsdbvisj","hzhdbcjhsdv",4655]
+
+// for each
+
+// a_dumb_array.forEach(element => {
+//     console.log(element);
+// });
+
+// manual for loop
+
+// for(i=0; i<a_dumb_array.length; i++){
+//     console.log(a_dumb_array[i]);
+    
+
+// }
+
+// random number
+
+// let random_number
+
+// ganerate_random_number = Math.floor(Math.random() * 100)
+// console.log(ganerate_random_number);
+
+// let id = Symbol('123')
+// let userId = Symbol('123')
+
+// console.log(id === userId);
---
+++ day2.js
@@ -0,0 +1,14 @@
+const friends = ["amir", "asad", "Inzamam", "Harsh", "Jamale"];
+
+let myOjects = {
+    name : "Musharraf",
+    rollNo : 2232332,
+    age : 23,
+};
+
+function myFunc(myOjects) {
+    console.log(myOjects.age);
+}
+myFunc(myOjects);
+
+console.log(typeof (new Date()));
